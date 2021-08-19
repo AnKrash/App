af8599432d10373094cd7bfffcbe8f929f39f538
ankrash <ankrash@ukr.net>
2021-08-19 14:01:25 +0300

    Made working method with resources

--- routes/api.php
+++ routes/api.php
@@ -31,18 +31,24 @@ use Illuminate\Support\Facades\Route;
 //
 //);
 
-Route::post('user/reset', [AuthController::class, 'resetUser']);
-Route::post('/user', [AuthController::class, 'store']);
-Route::post('/user/login', [AuthController::class, 'loginUser']);
-Route::post('/user/new_password', [AuthController::class, 'newPasswordUser']);
-Route::put('/user/update', [AuthController::class, 'updateUser']);
+//Route::post('user/reset', [AuthController::class, 'resetUser']);
+//Route::post('/user', [AuthController::class, 'store']);
+//Route::post('/user/login', [AuthController::class, 'loginUser']);
+//Route::post('/user/new_password', [AuthController::class, 'newPasswordUser']);
+//Route::put('/user/update', [AuthController::class, 'updateUser']);
 
 Route::prefix('user')->group(function () {
     Route::post('/login', [AuthController::class, 'loginUser']);
+    Route::post('/reset', [AuthController::class, 'resetUser']);
+    Route::post('/', [AuthController::class, 'store']);
+    Route::get('/', [AuthController::class, 'index']);
+    Route::post('/login', [AuthController::class, 'loginUser']);
+    Route::post('/new_password', [AuthController::class, 'newPasswordUser']);
+    Route::put('/update', [AuthController::class, 'updateUser']);
+    Route::delete('/{id}', [AuthController::class, 'delete']);
     Route::middleware('auth:api')->group(function () {
         Route::get('/{id}', [AuthController::class, 'show']);
+        //Route::delete('/{id}', [AuthController::class, 'delete']);
     });
 });
-
-Route::get('/users', [AuthController::class, 'index']);
 
